4ac1a2faf1d0908a0328d51ddc57bb175085ec0f
Sujan <sujan.dhenuja193@gmail.com>
2018-07-15 12:13:55 +0530

    Initial Commit

---
+++ designs.js
@@ -0,0 +1,24 @@
+function makeGrid() {
+    var height = document.getElementById('input_height').value;
+    var width = document.getElementById('input_width').value;
+    var table = document.getElementById('pixel_canvas');
+	// Table grid creation code :
+    table.innerHTML = '';
+    var tbody = document.createElement('tbody');
+    for (var i = 0; i < height; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < width; j++) {
+            var td = document.createElement('td');
+            td.appendChild(document.createTextNode(''));
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    table.appendChild(tbody);
+}
+
+//jQuery to change color of pixel when clicked.
+$('body').on('click', 'td', function() {
+	var color = document.getElementById('colorPicker').value;
+    $(this).css('background-color', color);
+});
