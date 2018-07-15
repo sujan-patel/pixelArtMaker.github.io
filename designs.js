de2c0a4b8e9bff17560cf8709de8dca9f833c934
Sujan Patel <Sujan Patel>
2018-07-15 13:18:27 +0530

    basic changes

--- designs.js
+++ designs.js
@@ -1,31 +1,26 @@
-//variables to get canvas element,height,width
+
 const c = document.getElementById('pixel_canvas');
 let temph = $("#input_height");
 let tempw = $("#input_width");
+let color = $("#colorPicker");
 
-//An event listener that comes into action when Submit button is clicked .
 $('#input_submit').click(function(e) {
     e.preventDefault();
     makeGrid();
 });
 
-let color = $("#colorPicker");
-
-//To make the grid pattern makegrid is called whenever button is clicked.
 function makeGrid() {
 
     c.innerHTML = '';
     let height = temph.val();
     let width = tempw.val();
 
-    //A function which fills color in the cell that was clicked and changes the color.
     let addEvent = function(cell) {
         cell.addEventListener('click', function() {
             cell.style.backgroundColor = color.val();
         });
     }
 
-    //An event listener such that whenever any cell is clicked it calls addEvent function and changes it's color.
     for (let i = 0; i < height; i++) {
         let row = c.insertRow(i);
         for (let j = 0; j < width; j++) {
